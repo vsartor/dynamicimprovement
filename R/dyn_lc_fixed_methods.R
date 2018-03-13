2ae6c84e2095a9a77e557ea859dfbb543fb2db66
Victhor Sartório <vct.sartor@gmail.com>
2018-03-13 17:38:00 -0300

    Extented plot method to include 1-step ahead forecasts

--- R/dyn_lc_fixed_methods.R
+++ R/dyn_lc_fixed_methods.R
@@ -11,21 +11,25 @@
 #' @param ages Vector with the ages if plotting 'beta' or 'alpha'. Optional.
 #' @param years Vector with the years if plotting 'kappa'. Optional.
 #' @param cred The credibility for the interval. Defaults to 0.95.
+#' @param age The age _index_ to check one-step ahead forecasts.
 #'
 #' @return A `ggplot2` object.
 #'
 #' @importFrom ggplot2 ggplot geom_line geom_point geom_ribbon aes
 #' @importFrom ggplot2 theme_bw xlab ylab
 #' @importFrom latex2exp TeX
+#' @importFrom stats qnorm
 #'
 #' @export
 plot.dyn_lc_fixed <- function(x, y, at = NULL, ages = NULL, years = NULL,
-                              cred = 0.95, ...)
+                              cred = 0.95, age = NULL, ...)
 {
     q0 <- qnorm(.5 + .5*cred)
+    if (is.null(ages)) ages <- 0:(nrow(x$data)-1)
+    if (is.null(years)) years <- 1:ncol(x$data)
 
     if (y == "beta") {
-        if (is.null(ages)) ages <- 0:(nrow(x$data)-1)
+        #-- Visualize beta at some time point --#
 
         m <- x$mean_b[ ,at]
         s <- x$sd_b[ ,at]
@@ -35,9 +39,10 @@ plot.dyn_lc_fixed <- function(x, y, at = NULL, ages = NULL, years = NULL,
             geom_line(col = 'darkblue') + geom_point(col = 'darkblue') +
             xlab('x') + ylab(TeX(sprintf("$\\beta_{%d,x}", at))) +
             geom_ribbon(aes(ymin = m - q0*s, ymax = m + q0*s),
-                        fill = 'darkblue', alpha = 0.25)
+                        fill = 'darkblue', alpha = 0.2)
+
     } else if (y == "alpha") {
-        if (is.null(ages)) ages <- 0:(nrow(x$data)-1)
+        #-- Visualize alpha at some timepoint --#
 
         m <- x$mean_a[ ,at]
         s <- x$sd_a[ ,at]
@@ -48,8 +53,9 @@ plot.dyn_lc_fixed <- function(x, y, at = NULL, ages = NULL, years = NULL,
             xlab('x') + ylab(TeX(sprintf("$\\alpha_{%d,x}", at))) +
             geom_ribbon(aes(ymin = m - q0*s, ymax = m + q0*s),
                         fill = 'darkblue', alpha = 0.25)
+
     } else if (y == "kappa") {
-        if (is.null(years)) years <- 1:ncol(x$data)
+        #-- Visualize kappa --#
 
         m <- x$mean_k
         s <- x$sd_k
@@ -61,6 +67,40 @@ plot.dyn_lc_fixed <- function(x, y, at = NULL, ages = NULL, years = NULL,
             geom_ribbon(aes(ymin = m - q0*s, ymax = m + q0*s),
                         fill = 'darkblue', alpha = 0.25)
 
+    } else if (y == "forecast") {
+        if (is.null(age)) {
+            #-- Visualize one-step ahead forecast for certain timepoint --#
+
+            tgt <- x$data[ ,at]
+
+            m <- x$f_mean[ ,at]
+            u <- x$f_upper[ ,at]
+            l <- x$f_lower[ ,at]
+
+            ggplot(NULL, aes(x = ages)) +
+                theme_bw() +
+                xlab('x') + ylab(TeX(sprintf("$Y_{x,%d}$", at))) +
+                geom_point(aes(y = tgt)) +
+                geom_line(aes(y = m), col = 'darkblue') +
+                geom_ribbon(aes(ymin = l, ymax = u),
+                            fill = 'darkblue', alpha = 0.25)
+        } else {
+            #-- Visualize one-step ahead forecast for a certain age --#
+
+            tgt <- x$data[age, ]
+
+            m <- x$f_mean[age, ]
+            u <- x$f_upper[age, ]
+            l <- x$f_lower[age, ]
+
+            ggplot(NULL, aes(x = years)) +
+                theme_bw() +
+                xlab('t') + ylab(TeX(sprintf("$Y_{%d,t}$", age))) +
+                geom_point(aes(y = tgt)) +
+                geom_line(aes(y = m), col = 'darkblue') +
+                geom_ribbon(aes(ymin = l, ymax = u),
+                            fill = 'darkblue', alpha = 0.25)
+        }
     } else {
         stop(sprintf("Invalid `y` value: %s", y))
     }
